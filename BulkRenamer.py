5db0409abbd7062e368e871d530ab635dc6cda2c
NatiG7 <NetanelGoral@gmail.com>
2024-11-18 12:06:04 +0200

    Added regexing option. Added sample txt files for messing around

--- BulkRenamer.py
+++ BulkRenamer.py
@@ -7,9 +7,10 @@
 # Code for rework
 
 import os
+import re
 
 
-def bulk_rename(folder_path, old_name_part, new_name_part):
+def bulk_rename(folder_path, pattern, replacement):
     '''
     This function receives a path variable, a part of an existing name of file(s)
     and the new name that will replace all found name parts.
@@ -17,11 +18,13 @@ def bulk_rename(folder_path, old_name_part, new_name_part):
     # Array for replacement matches
     matches = []
     
+    # Regex
+    regex = re.compile(pattern)
 
     # First run, scan for matches (like a regex)
     for filename in os.listdir(folder_path):
             # if a match found, name is added to the array
-            if old_name_part in filename:
+            if regex.search(filename):
                 matches.append(filename)
 
     # Report on found matches
@@ -44,17 +47,19 @@ def bulk_rename(folder_path, old_name_part, new_name_part):
             # Renaming all matches
             for filename in matches:
                 try:
-                    new_filename = filename.replace(old_name_part,new_name_part)
+                    new_filename = regex.sub(replacement,filename)
                     old_path = os.path.join(folder_path, filename)
                     new_path = os.path.join(folder_path, new_filename)
                     os.rename(old_path, new_path)
                     print(f"Renamed '{filename}' ---> '{new_filename}'\n")
                 except Exception as e:
-                    print(f"\nError occured while renaming '{filename}' :\n\t\t'{e}'")
+                    print(f"\nError occurred while renaming '{filename}' :\n\t\t'{e}'")
         else:
             print("\nOperation canceled by user.")
-    print("\n~~~~~ Proccess Completed ~~~~~")
+    print("\n~~~~~ Process Completed ~~~~~")
 
-folder = '../../Sandbox/Pit 1 (BulkRenamer)'
-number = 0
-bulk_rename(folder,'1','REDACTED')
+# Example usage
+folder = './Sandbox/Pit1'
+pattern = r'\d+'
+replacement = 'REDACTED'
+bulk_rename(folder,pattern,replacement)
